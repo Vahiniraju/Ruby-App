5cbf9614cc64e9f2f2225214316d38872cd797d5
Vahiniraju <durga.vahini575@gmail.com>
2018-03-27 19:37:23 -0400

    User - Logout feature

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -21,7 +21,8 @@ module SessionsHelper
   end
 
   def log_out
-
+    session.delete(:user_id)
+    @current_user = nil
   end
 
 end
--- test/integration/user_login_test.rb
+++ test/integration/user_login_test.rb
@@ -32,7 +32,19 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", login_path , count: 0
     assert_select "a[href=?]", user_path
     assert_select "a[href=?]", logout_path
+    delete logout_path
+    refute is_logged_in?
+    assert_redirected_to root_path
+    follow_redirect!
+    delete logout_path
+    follow_redirect!
+    assert_template root_path
+    assert_select "a[href=?]", login_path
+    assert_select "a[href=?]", logout_path,      count: 0
+    assert_select "a[href=?]", user_path(@user), count: 0
   end
+
+
 
 
 
