ded8ccddc171cac0646b5831b1bb3b79e3669bab
Vahiniraju <durga.vahini575@gmail.com>
2018-03-29 14:41:05 -0400

    Jquery gem

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -9,7 +9,10 @@
 //
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
-//
+
+//= require jquery3
 //= require rails-ujs
+//= require bootstrap
 //= require turbolinks
 //= require_tree .
+
