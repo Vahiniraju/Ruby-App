00e30c2f8c21b3759e6dbd117fc413ce52edac56
Vahiniraju <durga.vahini575@gmail.com>
2018-04-03 21:24:18 -0400

    Refactoring

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -1,14 +1,14 @@
 class AccountActivationsController < ApplicationController
 
   def edit
-    user = User.find_by(email: params[:email])
+    user = User.where(email: params[:email]).first
     if user && !user.active? && user.authenticated?(:activation, params[:id])
       user.activate
       login(user)
-      flash[:success] = "Account Activated!"
+      flash[:success] = "Account is successfully Activated!"
       redirect_to user
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = "The activation link is not valid"
       redirect_to root_path
     end
   end
