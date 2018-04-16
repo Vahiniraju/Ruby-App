b8bd30ca4687f6445823d4af346e7ca047638305
Vahiniraju <durga.vahini575@gmail.com>
2018-04-15 20:48:04 -0400

    Fixing tests

--- test/integration/new_userselection_test.rb
+++ test/integration/new_userselection_test.rb
@@ -5,9 +5,10 @@ class NewUserselectionTest < ActionDispatch::IntegrationTest
       @user = create :user
       @question = create :question
       @question2 = create :question
-      @correct_answer = @question.answers.where(is_correct: true).first.id
-      @user_selection = { user_selection: {user_id: @user.id,question_id: @question.id, answer_id: @correct_answer }}
-      @user_selection1 = { user_selection: {user_id: @question.user_id,question_id: @question.id, answer_id: @question.answers.sample.id }}
+      @question3 = create :question
+      @correct_answer = @question2.answers.where(is_correct: true).first.id
+      @user_selection = { user_selection: {question_id: @question2.id, answer_id: @correct_answer, tag: "General" }}
+      @user_selection1 = { user_selection: {question_id: @question.id, answer_id: @question.answers.sample.id }}
       @categories = ActsAsTaggableOn::Tag.all.collect(&:name)
       @tag =  @categories[0].to_s
   end
@@ -24,16 +25,21 @@ class NewUserselectionTest < ActionDispatch::IntegrationTest
     log_in_as(@user)
     get user_selections_path
     assert_template 'user_selections/new'
+    assert_select "a[href=?]", user_selections_path, count: 1
     post user_selections_path , params: @user_selection1, xhr: true
     assert_response :ok
     assert_template 'user_selections/_new'
-    post user_selections_path , params: @user_selection1, xhr: true
     assert_select_jquery :html, '#user_selection_answer' do
       assert_select "a[href=?]", user_selections_path, count: 1
     end
     get tag_path(@tag)
     assert_template "user_selections/_new"
     assert_select "a[href=?]" , tag_path(@tag), count: 1
+    post user_selections_path, params: @user_selection, xhr: true
+    puts response.body
+    assert_select_jquery :html, '#user_selection_answer' do
+      assert_select "a[href=?]", tag_path(@tag), count: 1
+    end
   end
 
 end
